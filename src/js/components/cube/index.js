0b55ee776f4a7213b516ab3dc5d5002fe9b19dd2
Krzysztof Growinski <k.growinski@outlook.com>
2018-02-20 12:27:49 +0100

    adding fixes and more complex example

--- src/js/components/cube/index.js
+++ src/js/components/cube/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as THREE from 'three';
+import TrackballControls from 'three-trackballcontrols';
 
 export default class Cube extends React.Component {
   constructor(props) {
@@ -14,39 +15,69 @@ export default class Cube extends React.Component {
     const width = this.mount.clientWidth;
     const height = this.mount.clientHeight;
 
+    const camera = new THREE.PerspectiveCamera( 60, width / height, 1, 1000 );
+    camera.position.z = 500;
+
     const scene = new THREE.Scene();
-    const light = new THREE.AmbientLight(0xffffff, 0.5);
-    scene.add(light);
+    scene.background = new THREE.Color( 0xcccccc );
+    scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+    const geometry = new THREE.SphereGeometry( 15, 15, 15 );
+    const material = new THREE.MeshLambertMaterial({color: '#ff00ff'});
+    for ( let i = 0; i < 500; i ++ ) {
+      let mesh = new THREE.Mesh( geometry, material );
+      mesh.position.x = ( Math.random() - 0.5 ) * 1000;
+      mesh.position.y = ( Math.random() - 0.5 ) * 1000;
+      mesh.position.z = ( Math.random() - 0.5 ) * 1000;
+      mesh.updateMatrix();
+      mesh.matrixAutoUpdate = false;
+      scene.add( mesh );
+    }
 
-    const light1 = new THREE.PointLight(0xffffff, 0.5);
-    scene.add(light1);
+
+    // lights
+    const light1 = new THREE.DirectionalLight( '#ffffff' );
+    light1.position.set( 1, 1, 1 );
+    scene.add( light1 );
+    const light2 = new THREE.DirectionalLight( '#000000' );
+    light2.position.set( -1, -1, -1 );
+    scene.add( light2 );
+    const light3 = new THREE.AmbientLight( '#222222' );
+    scene.add( light3 );
+    // renderer
+    const renderer = new THREE.WebGLRenderer( { antialias: false } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
 
 
-    const camera = new THREE.PerspectiveCamera(35, width / height, 0.1, 3000);
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-
-
-    // const geometry = new THREE.BoxGeometry(100, 100, 100);
-    const geometry = new THREE.SphereGeometry(50,10,10);
-
-
-    const material = new THREE.MeshLambertMaterial({ color: 0xff00ff });
-    const cube = new THREE.Mesh(geometry, material);
-
-    cube.position.set(0, 0, -1000);
-    camera.position.z = 4;
-    scene.add(cube);
-    renderer.setClearColor('#000000');
-    renderer.setSize(width, height);
+    const controls = new TrackballControls(camera);
+    controls.rotateSpeed = 1.0;
+    controls.zoomSpeed = 1.2;
+    controls.panSpeed = 0.8;
+    controls.noZoom = false;
+    controls.noPan = false;
+    controls.staticMoving = true;
+    controls.dynamicDampingFactor = 0.3;
+    controls.keys = [ 65, 83, 68 ];
+    controls.addEventListener( 'change', this.render );
 
     this.scene = scene;
     this.camera = camera;
     this.renderer = renderer;
     this.material = material;
-    this.cube = cube;
+    this.controls = controls;
+
+    window.addEventListener( 'resize', this.onWindowResize, false );
 
     this.mount.appendChild(this.renderer.domElement);
     this.start();
+  }
+
+  onWindowResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    this.controls.handleResize();
+    this.render();
   }
 
   componentWillUnmount() {
@@ -57,6 +88,7 @@ export default class Cube extends React.Component {
   start() {
     if (!this.frameId) {
       this.frameId = requestAnimationFrame(this.animate);
+
     }
   }
 
@@ -65,14 +97,12 @@ export default class Cube extends React.Component {
   }
 
   animate() {
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
-
     this.renderScene();
     this.frameId = window.requestAnimationFrame(this.animate);
   }
 
   renderScene() {
+    this.controls.update();
     this.renderer.render(this.scene, this.camera);
   }
 
